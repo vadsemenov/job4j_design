ee96c69d5ad6324c4a5885e72216b23c908c4288
Vadim Semenov <5587394@mail.ru>
2020-04-17 13:46:52 +0300

    5.1.1. Итератор для двухмерного массива int[][][#273638]

--- chapter_001/src/main/java/ru/job4j/iterator/JaggedArrayIterator.java
+++ chapter_001/src/main/java/ru/job4j/iterator/JaggedArrayIterator.java
@@ -1,52 +1,38 @@
 package ru.job4j.iterator;
 
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.stream.Stream;
+import java.util.NoSuchElementException;
 
 
 public class JaggedArrayIterator implements Iterator {
 
     private final int[][] values;
-    private final int arrayLength;
-    private int index = 0;
+    private int row = 0;
+    private int coloumn = 0;
 
     public JaggedArrayIterator(final int[][] values) {
         this.values = values;
-
-//        int tempIndex = 0;
-//        for (int i = 0; i < this.values.length; i++) {
-//            for (int j = 0; j < this.values[i].length; j++) {
-//                tempIndex++;
-//            }
-//        }
-//        this.arrayLength = tempIndex;
-
-        this.arrayLength = (int) Arrays.stream(this.values).flatMap(e -> Stream.of(e)).count() + 1;
-
+//        this.arrayLength = (int) Arrays.stream(this.values).flatMap(e -> Stream.of(e)).count() + 1;
     }
 
 
     @Override
     public boolean hasNext() {
-
-        return this.arrayLength > this.index;
+        return this.values.length > row;
     }
 
     @Override
     public Object next() {
-//        int tempIndex = 0;
-//        for (int i = 0; i < this.values.length; i++) {
-//            for (int j = 0; j < this.values[i].length; j++) {
-//                if (tempIndex == this.index) {
-//                    index++;
-//                    return values[i][j];
-//                }
-//                ++tempIndex;
-//            }
-//        }
-//
-//        return null;
-        return Arrays.stream(this.values).flatMapToInt(Arrays::stream).skip(index++).findFirst().getAsInt();
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        Object res = this.values[this.row][this.coloumn++];
+        if (values[row].length == coloumn) {
+            row++;
+            coloumn = 0;
+
+        }
+        return res;
+        //       return Arrays.stream(this.values).flatMapToInt(Arrays::stream).skip(index++).findFirst().getAsInt();
     }
 }
