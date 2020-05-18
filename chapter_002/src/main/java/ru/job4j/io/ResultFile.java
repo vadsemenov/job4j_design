784f746db9839f445a10e6c0ec878f992b2e5a3b
Vadim Semenov <5587394@mail.ru>
2020-05-18 19:26:43 +0300

    Запись в файл

--- chapter_002/src/main/java/ru/job4j/io/ResultFile.java
+++ chapter_002/src/main/java/ru/job4j/io/ResultFile.java
@@ -2,6 +2,9 @@ package ru.job4j.io;
 
 import java.io.FileOutputStream;
 
+/**
+ * Пример записи в файл
+ */
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
