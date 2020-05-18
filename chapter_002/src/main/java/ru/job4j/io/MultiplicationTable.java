d78713157b0e35283cbbf4b6747833cbe50c962d
Vadim Semenov <5587394@mail.ru>
2020-05-18 18:04:38 +0300

    0.1. FileOutputStream. [#273671]

---
+++ chapter_002/src/main/java/ru/job4j/io/MultiplicationTable.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder();
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                text.append(i + "x" + j + "=" + (i * j) + System.lineSeparator());
+            }
+            text.append(System.lineSeparator());
+        }
+        writeToFile("multTab", text);
+    }
+
+    public static void writeToFile(String fileName, StringBuilder text) {
+        try (FileOutputStream out = new FileOutputStream(fileName + ".txt")) {
+            out.write(text.toString().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
