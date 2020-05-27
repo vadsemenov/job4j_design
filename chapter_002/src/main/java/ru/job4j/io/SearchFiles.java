30ef0f67278bd160d3b42d7c96e5fc024370e4b7
Vadim Semenov <5587394@mail.ru>
2020-05-27 22:59:14 +0300

    4.1. Сканирование файловой системы. [#273668]

---
+++ chapter_002/src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,51 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class SearchFiles implements FileVisitor<Path> {
+    private Predicate<Path> predicate;
+    private List<Path> foundedPathes = new ArrayList<Path>();
+
+    public SearchFiles(Predicate<Path> pathPredicate) {
+        this.predicate = pathPredicate;
+    }
+
+    public static void main(String[] args) {
+    }
+
+    public List<Path> getPaths() {
+        return foundedPathes;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (predicate.test(file)) {
+            foundedPathes.add(file.getFileName());
+        }
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+}
