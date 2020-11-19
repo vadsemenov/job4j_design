8e5f84ed14d52b17449bdfd26758c77a424dcc00
Vadim Semenov <5587394@mail.ru>
2020-11-19 19:31:58 +0300

    6. Кодировка. [#273678]

--- chapter_002/src/main/java/ru/job4j/io/ConsoleChat.java
+++ chapter_002/src/main/java/ru/job4j/io/ConsoleChat.java
@@ -12,28 +12,44 @@ import java.util.Scanner;
  * Класс - консольный чат с ботом из текстового файла
  */
 public class ConsoleChat {
-    private List<String> text = new ArrayList<String>();
+    private List<String> textAnswer = new ArrayList<String>();
+    private List<String> textChat = new ArrayList<String>();
+
     private String answersFileName;
     private String targetFileName;
+
+    private static final String OUT = "закончить";
+    private static final String CONTINUE = "продолжить";
+    private static final String STOP = "стоп";
 
 
     public ConsoleChat(String answersFileName, String targetFileName) {
         this.answersFileName = answersFileName;
-        //Почему файл не читается? Как мне правильно задать путь?
-        //this.answersFileName = "./data/chatAnswers.txt";
+        //this.answersFileName = "./chapter_002/data/chatAnswers.txt";
         this.targetFileName = targetFileName;
 
         fillTheAnswers();
-        chat();
     }
 
     public void fillTheAnswers() {
         try (BufferedReader in = new BufferedReader(new FileReader(this.answersFileName))) {
-            in.lines().forEach(text::add);
+            in.lines().forEach(textAnswer::add);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    public void writeChat() {
+        if (textChat.size() > 0) {
+            try (FileOutputStream out = new FileOutputStream(this.targetFileName)) {
+                for (String str : textChat) {
+                    out.write(str.getBytes());
+                    out.write(System.lineSeparator().getBytes());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void chat() {
@@ -42,34 +58,31 @@ public class ConsoleChat {
         boolean isGetAnswer = true;
         String answer = "";
 
-        try (FileOutputStream out = new FileOutputStream(this.targetFileName)) {
-            while (!answer.equals("закончить")) {
-                answer = scanner.nextLine();
-                out.write(answer.toString().getBytes());
-                out.write(System.lineSeparator().getBytes());
+        while (!answer.equals(OUT)) {
+            answer = scanner.nextLine();
+            textChat.add(answer);
+            textChat.add(System.lineSeparator());
 
-                if (answer.equals("стоп")) {
-                    isGetAnswer = false;
-                } else if (answer.equals("продолжить")) {
-                    isGetAnswer = true;
-                } else if (answer.equals("закончить")) {
-                    break;
-                }
-
-                if (isGetAnswer) {
-                    answer = randomAnswer();
-                    System.out.println(answer);
-                    out.write(answer.getBytes());
-                    out.write(System.lineSeparator().getBytes());
-                }
+            if (answer.equals(STOP)) {
+                isGetAnswer = false;
+            } else if (answer.equals(CONTINUE)) {
+                isGetAnswer = true;
+            } else if (answer.equals(OUT)) {
+                break;
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+            if (isGetAnswer) {
+                answer = randomAnswer();
+                System.out.println(answer);
+                textChat.add(answer);
+                textChat.add(System.lineSeparator());
+            }
         }
+        writeChat();
     }
 
     public String randomAnswer() {
-        return text.get(new Random().nextInt(text.size()));
+        return textAnswer.get(new Random().nextInt(textAnswer.size()));
     }
 
 }
