a4d2062a93c76ba4f444251281eb58762f166b8c
Vadim Semenov <5587394@mail.ru>
2021-04-13 14:46:26 +0300

    1. Бот [#273686]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -6,8 +6,7 @@ import java.net.Socket;
 
 /**
  * Класс создает Сервер и Сокет для соединения.
- * Если клиент шлет Bye, то Сервер закрывается.
- */
+ * Если клиент шлет Exit, то Сервер закрывается. */
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
@@ -17,18 +16,38 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
+
                     while (!(str = in.readLine()).isEmpty()) {
-                        if (str.equals("GET /?msg=Bye HTTP/1.1")) {
+                        String[] line = str.split(" ");
+                        int index = line[1].lastIndexOf("=");
+                        String word = line[1].substring(index + 1);
+
+                        switch (word) {
+                            case ("Hello"):
+                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                out.write("Hello".getBytes());
+                                break;
+                            case ("Exit"):
+                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                out.write("Exit".getBytes());
+                                break;
+                            default:
+                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                out.write( word.getBytes());
+                                break;
+                        }
+
+                        if (word.equals("Exit")) {
                             break;
                         } else {
                             System.out.println(str);
-                        }
+                       }
                     }
-                    if (str.equals("GET /?msg=Bye HTTP/1.1")) {
-                        out.write("HTTP/1.1 200 Exit\r\n\\".getBytes());
+                    if (str.equals("GET /?msg=Exit HTTP/1.1")) {
+                        out.write("HTTP/1.1 200 Exit\r\n\r\n".getBytes());
                         break;
-                    }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                   }
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                 }
             }
         }
